7db1e25b4df0a546da9fe5e01d6ed6b9d72311dd
shouravrahman <shouravrahman006@gmail.com>
2023-10-31 07:15:06 +0600

    chore: remove chakra add daisyui

--- client/src/index.js
+++ client/src/index.js
@@ -2,10 +2,22 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
+import { Theme } from "react-daisyui";
+// import { store } from "./redux/store";
+// import { Provider } from "react-redux";
+
+// import MainLayout from "./layouts/MainLayout";
+
+// import "@fontsource-variable/inter";
+// import Home from "./pages/home";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
 	<React.StrictMode>
-		<App />
+		{/* <Provider store={store}> */}
+		<Theme dataTheme='synthwave'>
+			<App />
+		</Theme>
+		{/* </Provider> */}
 	</React.StrictMode>
 );
